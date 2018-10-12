3cf983c53a349efd452061a58bf13d5d6a023488
Marthox1999 <mateo.gregory@correounivalle.edu.co>
2018-10-12 08:00:23 -0500

    combolution implemented

--- 1rstDicomApproach.py
+++ 1rstDicomApproach.py
@@ -1,6 +1,7 @@
 import os
 import pydicom
 import numpy
+import math
 from matplotlib import pyplot, cm
 import tkinter as tk
 
@@ -98,6 +99,54 @@ def createHistogram():
 	pyplot.clf()
 	pyplot.plot(histogram)
 	pyplot.show()
+
+#Refleja la imagen para poder hacer la operacion de combolucion en las filas
+#y columnas de la imagen original que normamente no se le podria aplicar (bordes)
+
+def combolutionMiror (matriz, kernel):
+	return 0	
+
+#Se reduce el tamaño de la imagen en los bordes a los que no se les pudo aplicar 
+#la operacion combolucion
+def combolutionReduccion(matriz, kernel):
+	#la diferencia entre este y el combolutionIgnore es que este en vez de ignorar los bordes que no
+	#puede afectar los cambia a 0
+	#Prevent the manipulation of the original data
+	numOfRows=len(matriz[0])
+	numOfColumns=len(matriz)
+	ksize=len(kernel)
+	if(ksize%2 == 0):
+		return EOFError()	
+	neighbors = math.floor(len(kernel)/2)
+	newMatrix=[[0 for _ in range(numOfColumns-(2*neighbors))] for _ in range(numOfRows-(2*neighbors))]
+	#go through the matrix avoiding the borders necesaries depending on the neighbors
+	for i in range (neighbors,numOfColumns-neighbors):
+		for j in range (neighbors, numOfRows-neighbors):
+			sumKernel=0
+			for k in range(i-neighbors, i+neighbors):
+				for l in range(j-neighbors, j+neighbors):
+					sumKernel += matriz[k][l]*kernel[k-i-neighbors][l-j-neighbors]
+			newMatrix[i][j]=sumKernel
+	return newMatrix
+#No se hace ningun cambio sobre los bordes de la imagen
+def combolutionIgnore(matriz, kernel):
+	#Prevent the manipulation of the original data
+	newMatrix=matriz
+	numOfRows=len(matriz[0])
+	numOfColumns=len(matriz)
+	ksize=len(kernel)
+	if(ksize%2 == 0):
+		return EOFError()	
+	neighbors = math.floor(len(kernel)/2)
+	#go through the matrix avoiding the borders necesaries depending on the neighbors
+	for i in range (neighbors,numOfColumns-neighbors):
+		for j in range (neighbors, numOfRows-neighbors):
+			sumKernel=0
+			for k in range(i-neighbors, i+neighbors):
+				for l in range(j-neighbors, j+neighbors):
+					sumKernel += matriz[k][l]*kernel[k-i-neighbors][l-j-neighbors]
+			newMatrix[i][j]=sumKernel
+	return newMatrix
 
 
 da = ds.pixel_array
